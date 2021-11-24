46b7081c0618a0743a24329e0a1f3b05eb45b200
Upen <gautamupen99@gmail.com>
2021-11-24 22:47:18 +0545

    Commented

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,4 @@
-#Calculator Program
+#GUI Calculator Program
 from tkinter import *
 
 state_plu = ["O"]               #                 State Representation to check if operand button is pressed or not
